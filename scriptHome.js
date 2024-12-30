eb93ad67a521c17e97eaffa47fb51111e27f7a98
Fahad-WSC <r3dalpha91@gmail.com>
2024-12-30 21:17:27 +0700

    Delete scriptHome.js

--- scriptHome.js
+++
@@ -1,6 +0,0 @@
-function videoslider(videoFile) {
-        var videoPlayer = document.getElementById("videoPlayer");
-        videoPlayer.src = videoFile;
-        videoPlayer.load();
-        videoPlayer.play();
-  }
