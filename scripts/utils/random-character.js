7a71234a978d55d22343a64b763ce59eb3ef347a
Vann <evan.ashley11@gmail.com>
2025-01-09 22:18:37 +0700

    Update random-character.js
    add documentation

--- scripts/utils/random-character.js
+++ scripts/utils/random-character.js
@@ -2,3 +2,5 @@ let num = Math.random();
 if (num > 0.5) {
   document.querySelector(".character").classList.add("change-character");
 }
+//kode ini berjalan untuk memberikan peluang 50% untuk mengubah posisi gambar pada icon character dalam header
+//perubahan yang akan terjadi adalah mengubah icon character dari gambar traveler male ke female
